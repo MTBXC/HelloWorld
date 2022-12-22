3a49a10e20b800689a35495b7d62c56100f0ebff
Michał Korlatowicz <korlatowicz44@gmail.com>
2022-12-22 21:10:22 +0100

    Add project files.

---
+++ HelloWorld/Math.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Math
+    {
+        public double Add(double a, double b) => a + b;
+
+        public double Subtract(double a, double b) => a - b;
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class ProgramThatMakesDiffrentThings
+    {
+        public static void Main(string[] args)
+        {
+            double a = 20;
+            double b = 10;
+
+            Math math = new Math();
+
+            double addResult = math.Add(a, b);
+            double substractResult = math.Subtract(a, b);
+
+            Console.WriteLine(addResult.ToString());
+            Console.WriteLine(substractResult.ToString());
+        }
+    }
+}
